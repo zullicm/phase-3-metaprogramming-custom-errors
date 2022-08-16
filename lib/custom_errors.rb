6b60963145ecf869e91733458ab8588d05b41212
Christopher Marzulli <chriszulli15@gmail.com>
2022-08-16 16:37:17 -0400

    commit tree fd3bc3eac5a0cc4f00670c1a8824efe667fa792d

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,16 +6,25 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
   end
 
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
